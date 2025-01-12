22e0e7d9405ce0456388b12e56ca617faea9375f
Zeeshanx01 <zeeshanmunir13579@gmail.com>
2025-01-12 21:00:37 +0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,17 +1,18 @@
 import mongoose from "mongoose";
-const { Schema, model } = mongoose
+const { Schema, model } = mongoose;
 
 const UserSchema = new Schema({
-  email: { type: String, require: true },
+  email: { type: String, required: true },
   name: { type: String },
-  username: { type: String, require: true },
+  username: { type: String, required: true },
   profilepic: { type: String },
   coverpic: { type: String },
   razorpayid: { type: String },
   razorpaysecret: { type: String },
-  createdAt: { type: Date, default: Date.now() },
-  updatedAt: { type: Date, default: Date.now() }
-})
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
 
-// export default mongoose.models.User || model("User", UserSchema);
-export default mongoose.models.User || model("User", UserSchema);
+const User = mongoose.models.User || model("User", UserSchema);
+
+export default User;
